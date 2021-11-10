6848f2bf459a64b219a00c4d3da1707d060bf051
EC2 Default User <hiroshi.829f@gmail.com>
2021-11-10 11:35:04 +0000

    fix chapter02 for main

--- chapter02/work04.js
+++ chapter02/work04.js
@@ -1,15 +1,1 @@
 // インクリメントとデクリメント演算子2
-
-let numA = 1;
-
-const numX = numA++;
-
-console.log( numX ); // 1
-console.log( numA ); // 2
-
-let numB = 1;
-
-const numY = ++numB;
-
-console.log( numY ); // 2
-console.log( numB ); // 2
